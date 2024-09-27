6f4e2c80e53e4a5301c464bc9fbf86ca06f2808c
leosenpaihh <lakatosleonardo1123@gmail.com>
2024-09-27 22:31:43 +0200

    Feladatok

---
+++ kacsak.py
@@ -0,0 +1,13 @@
+#1. Kacsák (14 pont)
+#Story
+#Kiskoromban sokat voltam a tengerparton, de utána mindig megfürödtem,
+# mert befogta a bőröm a korom. Szerettem a tengerparton üldögélni és nézni a kacsákat,
+# ahogy a vízben pancsikolnak, kenyérrel nem etettem viszont őket,
+# mert a hangyák is szeretik a kenyeret és nem akartam, hogy ellepjék szegény kacsákat.
+
+#Készítsd el a kacsak függvényt, amely kiírja a képernyőre, hogy kacsaaaaaak!!!.
+
+def kacsak():
+    print("kacsaaaaaak!!!")
+
+#(kacsak()) tesztelés
