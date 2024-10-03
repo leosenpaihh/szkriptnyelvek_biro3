08e0660adda94598a326164a7a71621e088e9b5c
leosenpaihh <lakatosleonardo1123@gmail.com>
2024-10-03 20:26:02 +0200

    Feladatok (Nincs Story + Teszt)

---
+++ Oskori kalandok/idoutazas.py
@@ -0,0 +1,7 @@
+class Idogep:
+    def __init__(self, vissza_ido=1000):
+        self.vissza_ido = vissza_ido
+
+    def __str__(self):
+        return f"Az idogep ennyit fog visszautazni az idoben: {self.vissza_ido}"
+
